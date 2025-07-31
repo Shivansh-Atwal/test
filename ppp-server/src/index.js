e5dc50020a4900da3faa504d19e8a1a0c9001433
Shivansh Atwal <shivanshatwal37@gmail.com>
2025-07-31 21:02:54 +0530

    final

--- ppp-server/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pg_connection_1 = require("./connections/pg-connection");
-var app_1 = require("./app");
-(0, pg_connection_1.testConnection)();
-app_1.default.listen();
-app_1.default.initializeRoutes();
--- ppp-server/src/utils/openAi.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var openai_1 = require("openai");
-var API_KEY = process.env.OPENAI_API_KEY || '';
-var openai = new openai_1.default({
-    apiKey: API_KEY,
-    maxRetries: 2,
-});
-exports.default = openai;
